aa2cd41c0feb419c3f5c73878d50057e1d37d8fc
yukselcoding <ozdemryuksel@gmail.com>
2020-06-26 23:31:17 +0300

    01-first commit

---
+++ src/main/java/io/crud/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package io.crud.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
